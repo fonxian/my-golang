5556f079343afa91669fe6cacaf863fb07041bc5
fonxian <fonxian2015@163.com>
2019-09-25 07:22:25 +0800

    完成数据库连接

--- douban-crawler/douban.go
+++ douban-crawler/douban.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/parnurzeal/gorequest"
 	"log"
 )
 
 //https://mholt.github.io/json-to-go/
+//https://www.cnblogs.com/mafeng/p/8429983.html
 type DoubanMovie struct {
 	Subjects []struct {
 		Rate     string `json:"rate"`
@@ -21,6 +24,13 @@ type DoubanMovie struct {
 		IsNew    bool   `json:"is_new"`
 	} `json:"subjects"`
 }
+
+//TODO
+//翻页
+//休眠
+
+//入库
+//https://github.com/go-sql-driver/mysql
 
 func main() {
 	pageUrl := "https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=0"
@@ -40,6 +50,26 @@ func main() {
 	json.Unmarshal([]byte(body), &doubanMovie)
 	for _, movie := range doubanMovie.Subjects {
 		log.Printf("name:%s,rate:%s", movie.Title, movie.Rate)
+	}
+	db()
+}
+
+//连接数据库
+//https://blog.csdn.net/sinat_26682309/article/details/89738546
+func db() {
+	var id, name string
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/build")
+	rows, err := db.Query("select company_id,credit_code from t_build_main limit 100")
+	if err == nil {
+		for rows.Next() {
+			err := rows.Scan(&id, &name)
+			if err == nil {
+				log.Printf("id:%s,name:%s", id, name)
+			}
+
+		}
+	} else {
+		log.Println(err)
 	}
 
 }

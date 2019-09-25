88442a17aa088dd34f1c5b7c61659c3e576813e7
fonxian <fonxian2015@163.com>
2019-09-25 08:12:42 +0800

    完成简单爬取

--- douban-crawler/douban.go
+++ douban-crawler/douban.go
@@ -7,6 +7,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/parnurzeal/gorequest"
 	"log"
+	"strconv"
+	"time"
 )
 
 //https://mholt.github.io/json-to-go/
@@ -25,15 +27,26 @@ type DoubanMovie struct {
 	} `json:"subjects"`
 }
 
-//TODO
-//翻页
-//休眠
-
-//入库
-//https://github.com/go-sql-driver/mysql
+//数据库连接池
+//分模块
 
 func main() {
-	pageUrl := "https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=0"
+
+	var db, _ = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/build?charset=utf8")
+	for i := 0; i < 200; i++ {
+		crawler(db, 20*i)
+		//休眠2秒
+		time.Sleep(time.Duration(2) * time.Second)
+	}
+
+	db.Close()
+
+	// dbConnect(db)
+}
+
+func crawler(db *sql.DB, pageStart int) {
+
+	pageUrl := "https://movie.douban.com/j/search_subjects?type=movie&tag=%E8%B1%86%E7%93%A3%E9%AB%98%E5%88%86&sort=rank&page_limit=20&page_start=" + strconv.Itoa(pageStart)
 
 	req := gorequest.New()
 
@@ -48,28 +61,37 @@ func main() {
 
 	var doubanMovie DoubanMovie
 	json.Unmarshal([]byte(body), &doubanMovie)
+
 	for _, movie := range doubanMovie.Subjects {
 		log.Printf("name:%s,rate:%s", movie.Title, movie.Rate)
+		insert(db, movie.Title, movie.Rate)
 	}
-	db()
 }
 
 //连接数据库
 //https://blog.csdn.net/sinat_26682309/article/details/89738546
-func db() {
-	var id, name string
-	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/build")
-	rows, err := db.Query("select company_id,credit_code from t_build_main limit 100")
-	if err == nil {
-		for rows.Next() {
-			err := rows.Scan(&id, &name)
-			if err == nil {
-				log.Printf("id:%s,name:%s", id, name)
-			}
+// func dbConnect(db *sql.DB) {
 
-		}
-	} else {
-		log.Println(err)
-	}
+// 	var id, name string
+// 	rows, err := db.Query("select company_id,credit_code from t_build_main limit 100")
+// 	if err == nil {
+// 		for rows.Next() {
+// 			err := rows.Scan(&id, &name)
+// 			if err == nil {
+// 				log.Printf("id:%s,name:%s", id, name)
+// 			}
+
+// 		}
+// 	} else {
+// 		log.Println(err)
+// 	}
+
+// }
+
+//入库
+//https://github.com/go-sql-driver/mysql
+func insert(db *sql.DB, title string, rate string) {
+	stmt, _ := db.Prepare("INSERT t_douban SET title=?,rate=?")
+	stmt.Exec(title, rate)
 
 }
